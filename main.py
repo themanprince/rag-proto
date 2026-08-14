958f53b9143d87cf3fcefcc106535c40a60a4ed6
Prince Adigwe <princeadigwe29@gmail.com>
2026-08-14 03:20:14 +0000

    fixed issues likely causing error 'forbidden' with qdrant cloud

--- main.py
+++ main.py
@@ -54,16 +54,19 @@ def check_and_run_ingestion_pipeline():
 	all_splits = text_splitter.split_documents(docs)
 	add_log(f"Split documentation into {len(all_splits)} chunks.")
 	
-	if not os.environ.get("VOYAGE_API_KEY"):
-		add_log("Error! Needed env var not found")
-		raise HTTPException(status=500, detail="needed env var not found.")
+	required_API_keys = ["VOYAGE_API_KEY", "LANGSMITH_API_KEY", "QDRANT_API_KEY", "QDRANT_URL"]
+	for key in required_API_key:
+		if not os.environ.get(key):
+			add_log("Error! Needed env var not found")
+			raise HTTPException(status=500, detail="needed env var not found.")
 		
 	embeddings = VoyageAIEmbeddings(model="voyage-3")
 	qdrant = QdrantVectorStore.from_documents(
 		all_splits,
 		embeddings,
-		url="https://beed57ca-fb89-4356-b2a9-1307eb987f46.australia-southeast1-0.gcp.cloud.qdrant.io",
-		prefer_grpc = True,
+		url=os.environ["QDRANT_URL"],
+		api_key = os.environ["QDRANT_API_KEY"],
+		prefer_grpc = False,
 		collection_name = "VectorStore1"
 	)
 	add_log(f"Indexed {len(all_splits)} chunks")
